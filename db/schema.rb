4bb0e6370084df402af134bcd3c4e7b12cc00f3f
EstKabir <estbruno@gmail.com>
2016-02-04 19:51:52 -0200

    nested form pt 1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204004223) do
+ActiveRecord::Schema.define(version: 20160204183847) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20160204004223) do
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
+  create_table "crew_members", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "division",    limit: 255
+    t.integer  "starship_id", limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
   create_table "hackers", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -48,5 +56,11 @@ ActiveRecord::Schema.define(version: 20160204004223) do
 
   add_index "hackers", ["email"], name: "index_hackers_on_email", unique: true, using: :btree
   add_index "hackers", ["reset_password_token"], name: "index_hackers_on_reset_password_token", unique: true, using: :btree
+
+  create_table "starships", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
